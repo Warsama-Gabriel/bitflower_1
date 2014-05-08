190070ac6d1d7999f50ef285fc7b054b3020edec
chrome-tempest <srobleh@gmail.com>
2014-05-07 19:32:31 -0700

    added home controller for devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 
-def index
-  if user_signed_in?
-    redirect_to :controller =>'welcome', :action => 'index'
+  def index
+    if user_signed_in?
+      redirect_to :controller =>'welcome', :action => 'index'
+    end
   end
-end
 
 end
